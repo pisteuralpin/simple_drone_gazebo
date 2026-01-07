7ea94e6cf8b218bef7d67525e23af01558690ba0
Mathurin Roulier <roulier.mathurin@gmail.com>
2026-01-07 16:53:29 +0100

    ➕ Add GZ_SIM_RESOURCE_PATH setup for model and texture loading

--- launch/start_gz.launch.py
+++ launch/start_gz.launch.py
@@ -1,4 +1,5 @@
 from simple_launch import SimpleLauncher
+from launch.actions import SetEnvironmentVariable
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -9,11 +10,22 @@ full_world = sl.find('simple_drone_gazebo', 'flat.sdf')
 
 
 def launch_setup():
+    share = get_package_share_directory('simple_drone_description')
+    resource_root = os.path.dirname(share)
 
+    # Set GZ_SIM_RESOURCE_PATH to find models and textures
+    sl.add_action(SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=resource_root + ':' + os.environ.get('GZ_SIM_RESOURCE_PATH', '')
+    ))
+
+    # launch gazebo
     gz_args = '-r' if sl.arg('gui') else '-r -s'
 
+    # check if custom world file exists
     if os.path.exists(full_world):
         sl.gz_launch(full_world, gz_args)
+    # otherwise load default world
     else:
         sl.gz_launch(sl.find('simple_drone_gazebo', 'world.sdf'), gz_args)
         sl.save_gz_world(full_world, 5.)
